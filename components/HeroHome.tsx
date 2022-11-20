90b0f0bf57a4d632a42f4c2533696049eddf7390
anthonymatignon <anthony.matignon@gmail.com>
2022-11-20 10:49:41 +0100

    Add many pages

---
+++ components/HeroHome.tsx
@@ -0,0 +1,15 @@
+
+export default function HeroHome()
+{
+    return (
+        <div className={"bg-amber-50"}>
+            <h1 className="text-3xl font-bold">
+                Bienvenue chez PIZZA WEST
+            </h1>
+            <p>
+                Depuis 7 ans nous vous proposons des pizzas à emporter et en livraison sur Les Sables d'Olonne et sur Olonne sur mer.
+            </p>
+
+        </div>
+    );
+}
