e5578191df4603534bf46f2d9cfd8e5fba45a048
anthonymatignon <anthony.matignon@gmail.com>
2022-11-21 21:35:27 +0100

    Fix HeroHome section

--- components/HeroHome.tsx
+++ components/HeroHome.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function HeroHome() {
   return (
     <div className="font-display uppercase text-white bg-cust-orange rounded-3xl p-9">
@@ -6,11 +8,11 @@ export default function HeroHome() {
           <h1 className="text-5xl mb-8">Bienvenue chez PIZZA WEST</h1>
           <p className="text-3xl">
             Depuis 7 ans nous vous proposons des pizzas à emporter et en
-            livraison sur Les Sables d'Olonne et sur Olonne sur mer.
+            livraison sur Les Sables d&apos;Olonne et sur Olonne sur mer.
           </p>
         </div>
-        <img src="/hero_1.png" alt="Pizza" />
-        <img src="/hero_2.png" alt="Pizza" />
+        <Image src="/hero_1.png" alt="Pizza" />
+        <Image src="/hero_2.png" alt="Pizza" />
       </div>
     </div>
   )
